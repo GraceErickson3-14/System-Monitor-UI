527f3290c2b91ed2615cb2d3663e76d4bf43ef51
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-01-28 16:24:52 -0500

    Delete MongoDbSetting.cs

--- API/restapi/Models/MongoDbSetting.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace restapi.Models
-{
-    public class MongoDbSetting
-    {
-        public string ConnectionURI { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string CollectionName { get; set; } = null!;
-    }
-}
