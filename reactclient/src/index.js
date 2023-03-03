015c09544de22ec9a5e44997f908b5882177188b
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-03-03 11:25:49 -0500

    Update index.js

--- reactclient/src/index.js
+++ reactclient/src/index.js
@@ -5,11 +5,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+import 'fontsource-roboto';
+
+
+// Define your theme
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto',
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+<ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
+  </ThemeProvider>,
   </React.StrictMode>
 );
 
