9f318a31e891494df7643fb55c41dc28f23e6cf9
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-03-03 11:39:10 -0500

    Update Header.js

--- reactclient/src/Components/Header.js
+++ reactclient/src/Components/Header.js
@@ -1,26 +1,76 @@
 
 import React from 'react';
 import './Header.css'; 
-import { emphasize, styled } from '@mui/material/styles';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Chip from '@mui/material/Chip';
-import HomeIcon from '@mui/icons-material/Home';
-import { useLocation } from 'react-router-dom';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useState, useEffect } from 'react';
 import Breadcrumb from './Breadcrumb';
+import { createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Badge from './Badge';
+import Avatar from './Avatar';
+import logo from './VitalviewLogo.png';
 
 
-const Header = () => {
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#8DA0CB',
+      },
+    
+    },
+  });
+  
+
+
+
+  const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        const scrollTop = window.pageYOffset;
+        const shouldScroll = scrollTop > 0;
+        setIsScrolled(shouldScroll);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
     return (
-        <div>
-            <header className="header">
-                <h1>System Monitor</h1>
-                <div style={{ marginTop: '45px' }}>
-              <Breadcrumb/>
-              </div>
-            </header>
-        </div>
-    );
-}
+      
+      <div className={`header ${isScrolled ? 'scroll' : ''}`}>
+      <Box sx={{marginLeft:"50px"}}>
+         <img src={logo} alt="Logo" />
+      </Box>
 
-export default Header;
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
+       <div style={{ marginRight: '40px', marginTop:'-90px' }}>
+            {/*<Badge/>*/}
+       </div >
+
+       <div style={{ marginBottom: '20px', marginRight: "60px", marginTop:'-95px'}}>
+               {/*<Avatar/>*/}
+       </div>
+   
+       </Box>
+
+       <hr></hr>
+      
+       <Box sx={{ justifyContent: 'flex-start', marginTop: '-10px', marginLeft: '20px', marginBottom: '15px', height: '15px', width: '30%' }}>
+  <Breadcrumb />
+</Box>
+
+     
+
+   </div>
+
+
+   
+    );
+  };
+  
+  export default Header;
