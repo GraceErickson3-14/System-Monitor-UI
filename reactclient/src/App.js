77dc067ae6258defedecf41388e9a8ddd8da49c2
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-03-03 11:27:45 -0500

    Update App.js

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,27 +1,34 @@
 
-import React from "react";
+import React, { Component } from "react";
+import { useState } from 'react';
+
 import "./App.css";
 import Carousel from "./Components/Carousel";
 import Header from "./Components/Header";
 import { BrowserRouter as Router, Route,Routes, Switch, Link } from 'react-router-dom';
 import DetailedView from "./Components/DetailedView";
+import Container from '@mui/material/Container';
+
 
 function App() {
+    const [machine, setMachine] = useState('');
+
+ 
 
     return (
+        <div>
+      
         <Router>
-            <div>
                 <Header />
-                <div className="App center-div">
+                <div >
                 <Routes>
-                        <Route path="/" element={<Carousel />} />
-                        <Route path="/detailed-view/:machine" element={<DetailedView/>} />
+                        <Route path="/" element={<Carousel  />} />
+                        <Route path="/detailed-view/:machine" element={ <DetailedView />} />
                 </Routes>
             </div>
-          
-               
-            </div>
         </Router>
+      
+        </div>
     );
 }
 
