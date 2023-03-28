58cc9e1bb968bbdf241f7502b9af870f66c9708c
Chewy8234 <72936062+Chewy8234@users.noreply.github.com>
2023-03-28 10:50:01 -0400

    Add files via upload

---
+++ WindowsExporter/metrics.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Metric
+{
+    private string Name;
+    private string Value;
+    private string Mode;
+    private string Core;
+
+    public string Names
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+
+    public string Values
+    {
+        get { return Value; }
+        set { Value = value; }
+    }
+
+    public string Modes
+    {
+        get { return Mode; }
+        set { Mode = value; }
+    }
+}
+public class windowsCpuCstate
+{
+    public string Name { get; set; }
+    public string state { get; set; }
+    public double value { get; set; }
+
+}
+
+public class windowsCsPhysicalMemorybyte
+{
+    public double Value { get; set; }
+}
+
+public class windowsOSVirtual
+{
+    public double value { get; set; }
+}
+
+
+public class windowsSizeBytes
+{
+    private string Name;
+    private string Value;
+    private string Volume;
+
+    public string Names
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+    public string Values
+    {
+        get { return Value; }
+        set { Value = value; }
+    }
+    public string Volumes
+    {
+        get { return Volume; }
+        set { Volume = value; }
+    }
+}
+
