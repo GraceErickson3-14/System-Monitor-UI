8d3fe927038b365756f09fd476311224657a7096
Chewy8234 <72936062+Chewy8234@users.noreply.github.com>
2023-03-29 11:54:22 -0400

    Add files via upload

--- WindowsExporter/Program.cs
+++ WindowsExporter/Program.cs
@@ -8,29 +8,29 @@ using System.Text;
 
 class programMain
 {
-    private static HttpClient sharedClient = new()
-    {
-        BaseAddress = new Uri("https://localhost:7165/api/Metric"),
-    };
+    //private static HttpClient sharedClient = new()
+    //{
+    //    BaseAddress = new Uri("https://localhost:7165/api/Metric"),
+    //};
 
-    static async Task main(string[] args)
-    {
-        using (var httpClient = new HttpClient())
-        {
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://localhost:7165/api/Metric"),
-            };
-            request.Headers.Accept.ParseAdd("application/json");
+    //static async Task main(string[] args)
+    //{
+    //    using (var httpClient = new HttpClient())
+    //    {
+    //        var request = new HttpRequestMessage
+    //        {
+    //            Method = HttpMethod.Get,
+    //            RequestUri = new Uri("https://localhost:7165/api/Metric"),
+    //        };
+    //        request.Headers.Accept.ParseAdd("application/json");
 
 
-            using (var response = await httpClient.SendAsync(request))
-            {
-                string apiResponse = await response.Content.ReadAsStringAsync();
-            }
-        }
-    }
+    //        using (var response = await httpClient.SendAsync(request))
+    //        {
+    //            string apiResponse = await response.Content.ReadAsStringAsync();
+    //        }
+    //    }
+    //}
 
     static void Main(string[] args)
     {
@@ -82,44 +82,36 @@ class programMain
                 }
             }
             //windows_cs_physical_memory_bytes ComputerSystem.TotalPhysicalMemory
-            else if(lines.Contains("windows_cs_physical_memory_bytes"))
+            if (line.StartsWith("windows_cs_physical_memory_bytes"))
             {
                 var newCPUCstateSeconds = new windowsCsPhysicalMemorybyte();
-                newCPUCstateSeconds.Value = double.Parse(Regex.Match(line, @"\d+(\.\d+)?([eE][+-]?\d+)?").ToString());
+                newCPUCstateSeconds.Value = Regex.Match(line, @"\d+(\.\d+)?([eE][+-]?\d+)").Value;
+                Console.WriteLine(newCPUCstateSeconds.Value);
             }
+
             // windows_os_virtual_memory_bytes OperatingSystem.TotalVirtualMemorySize
-            else if(lines.Contains("windows_os_virtual_memory_bytes"))
+            if (line.StartsWith("windows_os_virtual_memory_bytes"))
             {
                 var newVirtual = new windowsOSVirtual();
-                newVirtual.value = double.Parse(Regex.Match(line, @"\d+(\.\d+)?([eE][+-]?\d+)?").ToString());
+                newVirtual.value = Regex.Match(line, @"\d+(\.\d+)?([eE][+-]?\d+)").Value;
+                Console.WriteLine(newVirtual.value);
             }
-        //windows_cpu_cstate_seconds_total Time spent in low - power idle state
-            else if(line.StartsWith("windows_cpu_cstate_seconds_total"))
+            //windows_cpu_cstate_seconds_total Time spent in low - power idle state
+            if (line.StartsWith("windows_cpu_cstate_seconds_total"))
             {
                 var cpuCstate = new windowsCpuCstate();
                 cpuCstate.Name = "windows_cpu_cstate_seconds_total";
                 cpuCstate.state = Regex.Match(line, "(c\\d+)").ToString();
-                if (cpuCstate.value != 0)
-                {
-                    cpuCstate.value = double.Parse(Regex.Match(line, @"\\d+\\.\\d+").ToString());
-                }
-                else 
-                {
-                    Match zeroValue = Regex.Match(line, @"\b0\b$"); 
-                    if(zeroValue.Success)
-                    {
-                        cpuCstate.value = double.Parse(zeroValue.Value);
-                    }
-                }
+                cpuCstate.value = double.Parse(Regex.Match(line, @"\d+(?:\.\d+)?(?=\s*$)").ToString());
             }
-            else if (line.StartsWith("windows_logical_disk_size_bytes"))
+            if (line.StartsWith("windows_logical_disk_size_bytes"))
             {
                 var windowDiskBytes = new windowsSizeBytes();
                 windowDiskBytes.Names = "windows_logical_disk_size_bytes";
                 windowDiskBytes.Values = Regex.Match(line, @"\d+\.\d+([eE][-+]?\d+)?").ToString();
                 windowDiskBytes.Volumes = Regex.Match(line, @"volume=""(.+?)""").ToString();
             }
-            else if (line.StartsWith("windows_logical_disk_free_bytes"))
+            if (line.StartsWith("windows_logical_disk_free_bytes"))
             {
                 var windowDiskBytes = new windowsSizeBytes();
                 windowDiskBytes.Names = "windows_logical_disk_size_bytes";
@@ -127,5 +119,7 @@ class programMain
                 windowDiskBytes.Volumes = Regex.Match(line, @"volume=""(.+?)""").ToString();
             }
         }
+
+        
     }
 }
