b7d8529b8297ed26a825ffb8c3f3a96b46eb8b5a
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-04-05 12:42:52 -0400

    Create TextField.js

---
+++ reactclient/src/Components/TextField.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+
+
+const TextField = ({placeholder}) => {
+    const ariaLabel = {
+      'aria-label': placeholder,
+    };
+  
+    return (
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1 },
+          marginRight: "200px",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Input sx={{ fontFamily: "Roboto", fontWeight: 600,  fontSize: '1.0625rem', }} placeholder={placeholder} inputProps={ariaLabel} />
+      </Box>
+    );
+  };
+  
+  export default TextField;
