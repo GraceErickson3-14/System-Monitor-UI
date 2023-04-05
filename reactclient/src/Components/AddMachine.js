c18af18f8d2b8bd8cf4ba07ae1995e6bb24ecfce
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-04-05 11:29:09 -0400

    Create AddMachine.js

---
+++ reactclient/src/Components/AddMachine.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import "./AddMachine.css";
+
+function AddMachine() {
+  return (
+    <div className = "comp" >
+    <Container maxWidth="xl" sx={{ margin: 'auto' }}>
+      {/* Baby Blue background*/}
+      <Box className="container_box"></Box>
+    </Container>
+    </div>
+  );
+}
+
+export default AddMachine;
