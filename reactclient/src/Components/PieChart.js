bca3a46546467d5cfaddb0798be514d2f60b2dee
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-04-05 11:59:38 -0400

    Update PieChart.js

--- reactclient/src/Components/PieChart.js
+++ reactclient/src/Components/PieChart.js
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, PieController, ArcElement, Legend, Tooltip } from "chart.js";
 import { Pie } from 'react-chartjs-2';
 import './PieChart.css';
@@ -6,23 +6,28 @@ import './PieChart.css';
 ChartJS.register(PieController, ArcElement, CategoryScale, LinearScale, Legend, Tooltip);
 
 class PieChart extends React.Component {
-    state = {
-        chartData: {
-            labels: ['User', 'System', 'Idle'],
-            datasets: [
-                {
-                    label: 'Utilization',
-                    data: [300, 50, 100],
-                    backgroundColor: ['rgba(30, 136, 229, 0.85)',
-                    'rgba(144, 202, 249', 'rgba(103, 58, 183, 0.85)'],
-                },
-            ],
-        },
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: {
+                labels: this.props.labels,
+                datasets: [
+                    {
+                        label: 'Utilization',
+                        data: this.props.data,
+                        backgroundColor: [ 'rgba(103, 58, 183, 0.85)','rgba(144, 202, 249)','rgba(30, 136, 229, 0.85)'],
+                    },
+                ],
+            },
+        };
+    }
 
     shouldComponentUpdate(nextProps, nextState) {
-        // Compare the current props and state to the next props and state
-        if (this.props.machineData === nextProps.machineData && this.state.chartData === nextState.chartData) {
+        if (
+            this.props.labels === nextProps.labels &&
+            this.props.data === nextProps.data &&
+            this.state.chartData === nextState.chartData
+        ) {
             return false;
         }
         return true;
