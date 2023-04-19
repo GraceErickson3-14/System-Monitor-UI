782b8334d369f55048f968f79d398052a332b6fc
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-04-18 20:23:47 -0400

    Update Tab.js

--- reactclient/src/Components/Tab.js
+++ reactclient/src/Components/Tab.js
@@ -1,21 +1,20 @@
+// Tabys.js
 import React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-
+import useTabState from './hooks/useTabState';
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#673ab9d9', // change this to the color you want
-      },
+  palette: {
+    primary: {
+      main: '#673ab9d9',
     },
-  });
+  },
+});
 
-
-export default function Tabys() {
-  const [value, setValue] = React.useState('one');
+const Tabys = ({ section, onStateChange = () => {} }) => {
+  const [value, setValue] = useTabState(`${section}_one`, onStateChange);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -23,31 +22,13 @@ export default function Tabys() {
 
   return (
     <ThemeProvider theme={theme}>
-    <Tabs
-      value={value}
-      onChange={handleChange}
-      aria-label="wrapped label tabs example"
-      centered
-    >
-      <Tab
-        value="one"
-        label="Month"
-        wrapped
-        style={{ textTransform: 'none', minWidth: 0 }}
-      />
-      <Tab
-        value="two"
-        label="Week"
-        wrapped
-        style={{ textTransform: 'none', minWidth: 0 }}
-      />
-      <Tab
-        value="three"
-        label="Day"
-        wrapped
-        style={{ textTransform: 'none', minWidth: 0 }}
-      />
-    </Tabs>
+      <Tabs value={value} onChange={handleChange} aria-label="wrapped label tabs example" centered>
+        <Tab value={`${section}_one`} label="Day" wrapped style={{ textTransform: 'none', minWidth: 0 }} />
+        <Tab value={`${section}_two`} label="Week" wrapped style={{ textTransform: 'none', minWidth: 0 }} />
+        <Tab value={`${section}_three`} label="Month" wrapped style={{ textTransform: 'none', minWidth: 0 }} />
+      </Tabs>
     </ThemeProvider>
   );
-}
+};
+
+export default Tabys;
