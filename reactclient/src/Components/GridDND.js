56c15a5a621a725dd7e5e828a527139f239b97f1
GraceErickson3-14 <83148824+GraceErickson3-14@users.noreply.github.com>
2023-04-18 20:03:01 -0400

    Create GridDND.js

---
+++ reactclient/src/Components/GridDND.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import logo from './VitalviewLogo.png';
+
+
+
+function GridDND() {
+
+  console.log("GridDND");
+  return (
+    <div> <img src={logo} alt="Logo" /></div>
+  
+  );
+
+}
+
+export default GridDND;
